99c16b415d19a2b65f4263451bf1dc0ba7aa8205
root <github@simonkowallik.com>
2020-12-13 17:07:03 +0000

    j2f5.py and boilerplate

---
+++ Makefile
@@ -0,0 +1,69 @@
+.PHONY: clean clean-test clean-pyc clean-build docs
+
+define BROWSER_PYSCRIPT
+import os, webbrowser, sys
+
+try:
+        from urllib import pathname2url
+except:
+        from urllib.request import pathname2url
+
+webbrowser.open("file://" + pathname2url(os.path.abspath(sys.argv[1])))
+endef
+export BROWSER_PYSCRIPT
+
+BROWSER := python -c "$$BROWSER_PYSCRIPT"
+
+clean: clean-build clean-pyc clean-test ## remove all build, test, coverage and Python artifacts
+
+clean-build: ## remove build artifacts
+	rm -fr build/
+	rm -fr dist/
+	rm -fr .eggs/
+	find . \( -path ./env -o -path ./venv -o -path ./.env -o -path ./.venv \) -prune -o -name '*.egg-info' -exec rm -fr {} +
+	find . \( -path ./env -o -path ./venv -o -path ./.env -o -path ./.venv \) -prune -o -name '*.egg' -exec rm -f {} +
+
+clean-pyc: ## remove Python file artifacts
+	find . -name '*.pyc' -exec rm -f {} +
+	find . -name '*.pyo' -exec rm -f {} +
+	find . -name '*~' -exec rm -f {} +
+	find . -name '__pycache__' -exec rm -fr {} +
+
+clean-test: ## remove test and coverage artifacts
+	rm -f .coverage
+	rm -f coverage.xml
+	rm -fr htmlcov/
+	rm -fr .pytest_cache
+	find . -name '.mypy_cache' -exec rm -fr {} +
+
+black:
+	black j2f5.py
+	black tests/*.py
+
+test:
+	pytest tests/
+
+tests: test
+
+coverage:
+	pytest --cov --cov-report=xml
+	coverage report -m
+	coverage html
+	$(BROWSER) htmlcov/index.html
+
+## generate Sphinx HTML documentation, including API docs
+docs:
+	rm -f docs/as3ninja.rst
+	rm -f docs/modules.rst
+	sphinx-apidoc -o docs/ as3ninja
+	$(MAKE) -C docs clean
+	$(MAKE) -C docs html
+	$(BROWSER) docs/_build/html/index.html
+
+dependencies-update:
+	poetry update
+
+dependencies-lock:
+	poetry lock
+
+dependencies: dependencies-update dependencies-lock
---
+++ j2f5.py
@@ -0,0 +1,91 @@
+# support reading templating start/end strings from the user supplied configuration
+# alter_context is called before j2_environment_params:
+# 1. Read and save context (based on user supplied configuration) in alter_context
+# 2. Read start/end strings from context in j2_environment_params
+# https://github.com/kolypto/j2cli/blob/26a67e9419d96b7f92871e8b93dba00306c5df0b/j2cli/cli.py#L183-L186
+_context = dict()
+
+# constants
+_TEMPLATING_NAMESPACE = "templating_settings"
+
+
+def ansible_filters():
+
+    _ansible_filters = dict()
+
+    try:
+        from ansible.plugins.filter import core as ansible_core_filters
+        from ansible.plugins.filter import urls as ansible_urls_filters
+        from ansible.plugins.filter import urlsplit as ansible_urlsplit_filters
+        from ansible.plugins.filter import mathstuff as ansible_mathstuff_filters
+
+        _ansible_filters.update(ansible_core_filters.FilterModule().filters())
+        _ansible_filters.update(ansible_urls_filters.FilterModule().filters())
+        _ansible_filters.update(ansible_urlsplit_filters.FilterModule().filters())
+        _ansible_filters.update(ansible_mathstuff_filters.FilterModule().filters())
+    except ImportError:
+        pass
+
+    return _ansible_filters
+
+
+def alter_context(context):
+    """modify jinja2 context"""
+    # register context in _context for later use in j2_environment_params
+    global _context
+    _context = context
+
+    # start other modifications below
+    return context
+
+
+def extra_filters():
+    """extra jinja2 filters"""
+    _extra_filters = dict()
+
+    # load ansible filters when ansible is available
+    _extra_filters.update(ansible_filters())
+
+    return _extra_filters
+
+
+def j2_environment_params():
+    """jinja2 environment parameters"""
+
+    custom_params = dict(
+        # use this dict to further customize or overwrite params
+    )
+
+    templating_params = dict(
+        # Custom block start/end strings
+        block_start_string=_context.get(_TEMPLATING_NAMESPACE, {}).get(
+            "block_start_string"
+        ),
+        block_end_string=_context.get(_TEMPLATING_NAMESPACE, {}).get(
+            "block_end_string"
+        ),
+        # Custom variable start/end strings
+        variable_start_string=_context.get(_TEMPLATING_NAMESPACE, {}).get(
+            "variable_start_string"
+        ),
+        variable_end_string=_context.get(_TEMPLATING_NAMESPACE, {}).get(
+            "variable_end_string"
+        ),
+        # Custom comment start/end strings
+        comment_start_string=_context.get(_TEMPLATING_NAMESPACE, {}).get(
+            "comment_start_string"
+        ),
+        comment_end_string=_context.get(_TEMPLATING_NAMESPACE, {}).get(
+            "comment_end_string"
+        ),
+        # Remove whitespace around blocks
+        trim_blocks=True,
+        lstrip_blocks=True,
+        # Keep \n at the end of a file
+        keep_trailing_newline=True,
+    )
+    # remove None's
+    templating_params = {k:v for (k,v) in templating_params.items() if v is not None}
+
+    templating_params.update(custom_params)
+    return templating_params
